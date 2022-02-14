562974ec80e35ec0f57e23b6e39af950f3d699a2
DhruvJ9922 <dhruvj9922@gmail.com>
2022-02-13 22:32:57 -0500

    incorporated PR comments

---
+++ contrib/cr3bp_DhruvJ/untitled0.py
@@ -0,0 +1,30 @@
+"""
+Created on Fri Feb 11 23:20:21 2022
+
+@author: Dhruv Jain, Multi-Body Dynamics Research Group, Purdue University
+"""
+
+
+struct or class of bodies_cr3bp
+mu, lstar, tstar
+
+class cr3bp_traj:
+    pass bodies
+   states
+   stm
+   time
+   tevents
+   yevents
+
+targ prop
+   inttol
+   convtol
+   nmax
+   intmethod
+   
+fam_cont
+natural or pseudo arc len
+ stepoff
+ 
+ 
+ 
